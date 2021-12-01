aa4f901d0af1819e55448e73e5e45377a9e17a2b
jkulka <jkulka@hawaii.edu>
2021-11-30 18:18:36 -1000

    replaced dialogflow web demo with the messenger

--- app/imports/ui/components/Chatbot.jsx
+++ app/imports/ui/components/Chatbot.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
-import { Popup, Button } from 'semantic-ui-react';
 
 /** The Chatbot that appears at the bottom corner of the page. From Google's DialogFlow */
 class Chatbot extends React.Component {
   render() {
-    const style = {
-      marginBottom: '2em',
-      marginRight: '2em',
-    };
     return (
-      <Popup
-        trigger={<Button primary content='Click me for help!'/>}
-        content={<iframe width="350" height="430" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/902db386-09c4-42fd-8e20-4c3aaa26070a"/>}
-        on='click'
-        position='top right'
-        inverted
-        style={style}
-      />
+      <div>
+        <df-messenger
+          chat-icon="https:&#x2F;&#x2F;mauitime.com&#x2F;wp-content&#x2F;uploads&#x2F;2019&#x2F;04&#x2F;oha-logo-courtersy-office-of-hawaiian-affairs.png"
+          intent="WELCOME"
+          chat-title="oha-chatbot"
+          agent-id="902db386-09c4-42fd-8e20-4c3aaa26070a"
+          language-code="en"
+        ></df-messenger>
+      </div>
     );
   }
 }
